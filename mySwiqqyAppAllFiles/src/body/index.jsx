e21e0a268e9376e1b327e2773c5333c4324463b9
dibborah <90625406+dibborah@users.noreply.github.com>
2023-10-03 22:39:35 +0530

    Update index.jsx

--- mySwiqqyAppAllFiles/src/body/index.jsx
+++ mySwiqqyAppAllFiles/src/body/index.jsx
@@ -1,272 +1,68 @@
-import React from 'react'
-import "./style.css"
-import one from "./img/one.jpg"
-import two from "./img/two.jpg"
-import three from "./img/three.jpg"
-import four from "./img/four.jpg"
-import five from "./img/five.jpg"
-import six from "./img/six.jpg"
-import seven from "./img/seven.jpg"
-import eight from "./img/eight.jpg"
-import nine from "./img/nine.jpg"
-import ten from "./img/ten.jpg"
-import eleven from "./img/eleven.jpg"
-import twelve from "./img/twelve.jpg"
-
-export default function index(){
-    return(
-        <>
-            {/* for this main__Container use grid */}
-                <section className="main__Container" id="main">
-
-                        {/* for this productlist use flex */}
-                            <div className="product-list">
-
-                                <div className="below__Section">
-
-                                    <img src={one} alt=""/>
-
-                                    <div className="title">
-                                       <h3>GOBI EDLI</h3>
-                                    </div>
-
-                                    <div className="rating">
-                                        <span>*****</span>
-                                    </div>
-
-                                    <div className="price">
-                                        <p>Rs 155</p>
-                                    </div>
-                                    <div className="button">
-                                        <button>Add To Cart</button>
-                                    </div>
-
-                                </div>
-                                <div className="below__Section">
-
-                                    <img src={two} alt=""/>
-
-                                    <div className="title">
-                                       <h3>Hamburger</h3>
-                                    </div>
-
-                                    <div className="rating">
-                                        <span>****</span>
-                                    </div>
-
-                                    <div className="price">
-                                        <p>Rs 120</p>
-                                    </div>
-                                    <div className="button">
-                                        <button>Add To Cart</button>
-                                    </div>
-
-                                </div>
-                                <div className="below__Section">
-
-                                    <img src={three} alt=""/>
-
-                                    <div className="title">
-                                       <h3>French Fries</h3>
-                                    </div>
-
-                                    <div className="rating">
-                                        <span>**</span>
-                                    </div>
-
-                                    <div className="price">
-                                        <p>Rs 100</p>
-                                    </div>
-                                    <div className="button">
-                                        <button>Add To Cart</button>
-                                    </div>
-
-                                </div>
-                                <div className="below__Section">
-
-                                    <img src={four} alt=""/>
-
-                                    <div className="title">
-                                       <h3>Cheese Burger</h3>
-                                    </div>
-
-                                    <div className="rating">
-                                        <span>*****</span>
-                                    </div>
-
-                                    <div className="price">
-                                        <p>Rs 200</p>
-                                    </div>
-                                    <div className="button">
-                                        <button>Add To Cart</button>
-                                    </div>
-
-                                </div>
-                                <div className="below__Section">
-
-                                    <img src={five} alt=""/>
-
-                                    <div className="title">
-                                       <h3>Vegetable Burger</h3>
-                                    </div>
-
-                                    <div className="rating">
-                                        <span>****</span>
-                                    </div>
-
-                                    <div className="price">
-                                        <p>Rs 180</p>
-                                    </div>
-                                    <div className="button">
-                                        <button>Add To Cart</button>
-                                    </div>
-
-                                </div>
-                                <div className="below__Section">
-
-                                    <img src={six} alt=""/>
-
-                                    <div className="title">
-                                       <h3>Cheese Chap</h3>
-                                    </div>
-
-                                    <div className="rating">
-                                        <span>*****</span>
-                                    </div>
-
-                                    <div className="price">
-                                        <p>Rs 200</p>
-                                    </div>
-                                    <div className="button">
-                                        <button>Add To Cart</button>
-                                    </div>
-
-                                </div>
-                                <div className="below__Section">
-
-                                    <img src={seven} alt=""/>
-
-                                    <div className="title">
-                                       <h3>Fruit Salad</h3>
-                                    </div>
-
-                                    <div className="rating">
-                                        <span>***</span>
-                                    </div>
-
-                                    <div className="price">
-                                        <p>Rs 99</p>
-                                    </div>
-                                    <div className="button">
-                                        <button>Add To Cart</button>
-                                    </div>
-
-                                </div>
-                                <div className="below__Section">
-
-                                    <img src={eight} alt=""/>
-
-                                    <div className="title">
-                                       <h3>Cucumbar Sabji</h3>
-                                    </div>
-
-                                    <div className="rating">
-                                        <span>****</span>
-                                    </div>
-
-                                    <div className="price">
-                                        <p>Rs 180</p>
-                                    </div>
-                                    <div className="button">
-                                        <button>Add To Cart</button>
-                                    </div>
-
-                                </div>
-                                <div className="below__Section">
-
-                                    <img src={nine} alt=""/>
-
-                                    <div className="title">
-                                       <h3>Tandori Biryani</h3>
-                                    </div>
-
-                                    <div className="rating">
-                                        <span>*****</span>
-                                    </div>
-
-                                    <div className="price">
-                                        <p>Rs 350</p>
-                                    </div>
-                                    <div className="button">
-                                        <button>Add To Cart</button>
-                                    </div>
-
-                                </div>
-                                <div className="below__Section">
-
-                                    <img src={ten} alt=""/>
-
-                                    <div className="title">
-                                       <h3>Donut</h3>
-                                    </div>
-
-                                    <div className="rating">
-                                        <span>***</span>
-                                    </div>
-
-                                    <div className="price">
-                                        <p>Rs120</p>
-                                    </div>
-                                    <div className="button">
-                                        <button>Add To Cart</button>
-                                    </div>
-
-                                </div>
-                                <div className="below__Section">
-
-                                    <img src={twelve} alt=""/>
-
-                                    <div className="title">
-                                       <h3>Chinese Food</h3>
-                                    </div>
-
-                                    <div className="rating">
-                                        <span>**</span>
-                                    </div>
-
-                                    <div className="price">
-                                        <p>Rs 130</p>
-                                    </div>
-                                    <div className="button">
-                                        <button>Add To Cart</button>
-                                    </div>
-
-                                </div>
-                                <div className="below__Section">
-
-                                    <img src={eleven} alt=""/>
-
-                                    <div className="title">
-                                       <h3>Plain Rice</h3>
-                                    </div>
-
-                                    <div className="rating">
-                                        <span>*****</span>
-                                    </div>
-
-                                    <div className="price">
-                                        <p>Rs 400</p>
-                                    </div>
-                                    <div className="button">
-                                        <button>Add To Cart</button>
-                                    </div>
-
-                                </div>
-
-                            </div>
-
-                </section>    
-              
-        </>
-    )
+import React, { useEffect, useState } from "react";
+import "./style.css";
+import Axios from "axios";
+import {Spin} from 'antd';
+import { Content } from "antd/es/layout/layout";
+
+
+export default function Index() {
+    const [images, setImages] = useState([]);
+    const [check, setCheck] = useState(true);
+   
+
+  const fetchAPI = async () => {
+    const response = await Axios.get(
+      "https://api.unsplash.com/photos/?client_id=uqr-0EE5-49f0cQTQEnmvP7RhmFMqc_Smdtgj0EUxMs"
+    );
+    console.log(response.data);
+    const data = await response.data;
+    setImages(data);
+  };
+
+  useEffect(() => {
+    fetchAPI();
+  }, []);
+  
+  useEffect(() => {
+    setCheck(false);
+  },[check])
+
+  return (
+    <>
+      {/* for this main__Container use grid */}
+      <section className="main__Container" id="main">
+
+        {/* for this productlist use flex */}
+        {check ? <Spin className="spinner"/> : 
+        <div className="product-list">
+          {images?.map((item, key) => (
+            <div className="below__Section" key={key} >
+              <img src={item.urls.small} alt="Unsplash Images" />
+                <div className="desc1">
+                <div className="title">
+                  <h1>{item.user.username}</h1>
+                </div>
+
+                <div className="price">
+                  <p>Rs {item.likes + 100}</p>
+                </div>
+                </div>
+                
+                <div className="desc2">
+                <div className="rating">
+                  <span>*****</span>
+                </div>
+
+                <div className="button">
+                  <button>Add To Cart</button>
+                </div>
+                </div>
+            </div>
+          ))}
+        </div>}
+            
+      </section>
+    </>
+  );
 }
+
